2eac0dcb915e14a30abd8b8a05693a2df757910f
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-12-19 20:30:25 -0600

    Try deployment to JetStream2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import * as dotenv from 'dotenv';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+export default ({ mode }) => {
+	if (mode !== 'production') {
+		dotenv.config();
+	}
+	return defineConfig({
+		plugins: [sveltekit()]
+	});	
+};
