97e0994b74f3e93367ae1b4230e9cbf8e977041b
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2024-12-24 16:14:54 -0600

    WIP-#1: Add Audio and Data Message Preview

--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -19,6 +19,7 @@
 
 	let { data }: { data: PageData } = $props();
 	let videoContainer: HTMLDivElement;
+	let audioContainer: HTMLDivElement;
 
 	const subscribedAudioTracks = $state<Record<string, TrackSubscription>>({});
 	const subscrbedVideoTracks = $state<Record<string, TrackSubscription>>({});
@@ -30,9 +31,6 @@
 		publication: livekit.RemoteTrackPublication,
 		participant: livekit.RemoteParticipant
 	) {
-		// remove tracks from all attached elements
-		// const elements = track.detach();
-		// elements.forEach((element) => element.remove());
 		if (track.kind === livekit.Track.Kind.Video) {
 			if (track.sid && subscrbedVideoTracks[track.sid]) {
 				delete subscrbedVideoTracks[track.sid];
@@ -52,7 +50,8 @@
 		if (track.kind === livekit.Track.Kind.Video) {
 			if (track.sid && !subscrbedVideoTracks[track.sid]) {
 				subscrbedVideoTracks[track.sid] = {
-					participant: participant.name || participant.identity,
+					participant:
+						(participant.name || participant.identity) + `(${publication.track?.source})`,
 					track,
 					kind: track.kind
 				};
@@ -60,7 +59,8 @@
 		} else if (track.kind === livekit.Track.Kind.Audio) {
 			if (track.sid && !subscribedAudioTracks[track.sid]) {
 				subscribedAudioTracks[track.sid] = {
-					participant: participant.name || participant.identity,
+					participant:
+						(participant.name || participant.identity) + `(${publication.track?.source})`,
 					track,
 					kind: track.kind
 				};
@@ -71,7 +71,15 @@
 	function showIntialTracks(room: livekit.Room) {
 		room.remoteParticipants.forEach((participant) => {
 			participant.videoTrackPublications.forEach((publication) => {
-				handleTrackSubscribed(publication.track!, publication, participant);
+				if (publication.track) {
+					handleTrackSubscribed(publication.track!, publication, participant);
+				}
+			});
+
+			participant.audioTrackPublications.forEach((publication) => {
+				if (publication.track) {
+					handleTrackSubscribed(publication.track!, publication, participant);
+				}
 			});
 		});
 	}
@@ -94,11 +102,13 @@
 		room.on('trackSubscribed', handleTrackSubscribed);
 		room.on('trackUnsubscribed', handleTrackUnsubscribed);
 		room.on('disconnected', () => {
-			goto('/admin');
+			goto('/admin', {
+				invalidateAll: true
+			});
 		});
 	});
 
-	function attach(node: HTMLVideoElement) {
+	function attachVideo(node: HTMLVideoElement) {
 		$effect(() => {
 			if (node) {
 				const track = subscrbedVideoTracks[node.id];
@@ -113,29 +123,60 @@
 			}
 		});
 	}
+
+	function attachAudio(node: HTMLAudioElement) {
+		$effect(() => {
+			if (node) {
+				const track = subscribedAudioTracks[node.id];
+				if (track) {
+					track.track.attach(node);
+					node.muted = true;
+				}
+				return () => {
+					if (track) {
+						track.track.detach(node);
+					}
+				};
+			}
+		});
+	}
 </script>
 
 <div
-	class="max-w-8xl mx-auto mb-40 flex flex-col gap-2 overflow-y-auto px-6 pb-0 pt-4 lg:px-6 lg:pt-4"
+	class="max-w-8xl mx-auto mb-60 flex h-full flex-col gap-2 overflow-auto overflow-y-auto px-6 pb-0 pt-4 lg:px-6 lg:pt-4"
 >
-	<h2 class="font-semibold text-gray-900 md:text-2xl dark:text-gray-300">
-		Welcome to Session {data.session.name}!, {data.token.identity}
-	</h2>
+	<div class="flex flex-row justify-between">
+		<h2 class="font-semibold text-gray-900 md:text-2xl dark:text-gray-300">
+			Welcome to Session {data.session.name}!, {data.token.identity}
+		</h2>
+		<div class="flex flex-row items-center justify-between text-center">
+			<h3 class="font-semibold text-gray-900 md:text-xl dark:text-gray-300">{fullScreenText}</h3>
+			<div class="h-5 w-5">
+				<button class="text-gray-900 dark:text-gray-300">
+					<MdFullscreen role="button" class="p-2 text-xs text-black dark:text-gray-300" />
+				</button>
+			</div>
+			<Tooltip class="dark:bg-gray-900" placement="bottom-start"
+				>Enter Full Screen(grid) View [todo]</Tooltip
+			>
+		</div>
+	</div>
+
 	<h3 class="font-semibold text-gray-900 md:text-xl dark:text-gray-300">Video Streams</h3>
 	<div
 		class="grid min-h-0 grid-cols-1 justify-start gap-2 p-2 md:grid-cols-3 lg:grid-cols-4"
 		bind:this={videoContainer}
 	>
 		{#each Object.entries(subscrbedVideoTracks) as [id, trackInfo]}
-			<div class="flex flex-col justify-between bg-blue-200 dark:bg-gray-700" style="height:300px;">
+			<div class="flex flex-col justify-between bg-gray-200 dark:bg-gray-700" style="height:300px;">
 				<FullScreen let:onRequest let:onExit>
 					<div class="flex-1">
 						<!-- svelte-ignore a11y_media_has_caption -->
-						<video use:attach class="h-full w-full" {id}></video>
+						<video use:attachVideo class="h-full w-full" {id}></video>
 					</div>
 					<div class="flex flex-row items-center justify-between text-center">
 						<span class="p-2 text-gray-900 dark:text-gray-300">{trackInfo.participant}</span>
-						<div class="h-5 w-5">
+						<div class="h-5 w-5 text-gray-900 dark:text-gray-300">
 							<button
 								onclick={() => {
 									onRequest();
@@ -143,7 +184,7 @@
 									fullScreenText = trackInfo.participant;
 								}}
 							>
-								<MdFullscreen role="button" class="p-2 text-xs text-gray-900 dark:text-gray-300" />
+								<MdFullscreen role="button" class="p-2 text-xs" />
 							</button>
 							<Tooltip class="dark:bg-gray-900" placement="bottom-end">Full Screen View</Tooltip>
 						</div>
@@ -152,5 +193,23 @@
 			</div>
 		{/each}
 	</div>
-	<!-- <h3 class="font-semibold text-gray-900 md:text-xl dark:text-gray-300">Audio Steams</h3> -->
+	<h3 class="font-semibold text-gray-900 md:text-xl dark:text-gray-300">Audio Steams</h3>
+	<div
+		class="grid min-h-0 grid-cols-1 justify-start gap-2 md:grid-cols-3 lg:grid-cols-4"
+		bind:this={audioContainer}
+	>
+		{#each Object.entries(subscribedAudioTracks) as [id, trackInfo]}
+			<div class="flex flex-col justify-between bg-gray-200 dark:bg-gray-700" style="height:100px;">
+				<div class="flex-1 p-2">
+					<!-- svelte-ignore a11y_media_has_caption -->
+					<audio use:attachAudio class="h-full w-full dark:bg-gray-700" controls {id}></audio>
+				</div>
+				<div class="flex flex-row items-center justify-between text-center dark:bg-gray-700">
+					<span class="p-2 text-gray-900 dark:text-gray-300">{trackInfo.participant}</span>
+				</div>
+			</div>
+		{/each}
+	</div>
+	<h3 class="font-semibold text-gray-900 md:text-xl dark:text-gray-300">Text Streams</h3>
+	<div class="mb-10"></div>
 </div>
